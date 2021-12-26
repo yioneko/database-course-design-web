f1476e8995dc050a8a0a271c8114706a86c49337
yioneko <yioneko@outlook.com>
2021-12-26 11:55:36 +0800

    chore: move env out of nextjs config

--- next.config.js
+++ next.config.js
@@ -4,14 +4,4 @@ const withTM = require("next-transpile-modules")([
 ]);
 module.exports = withTM({
   reactStrictMode: true,
-  env: {
-    MYSQL_HOST: "",
-    MYSQL_USER: "",
-    MYSQL_PASSWORD: "",
-    MYSQL_DATABASE: "",
-    SALT_LENGTH: 16,
-    HASH_ALGORITHM: "sha512",
-    JWT_SECRET: "",
-    HOST: "http://localhost:3000",
-  },
 });
